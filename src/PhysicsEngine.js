31001bdb8795803a40f5d0a9db710d867b42f5b5
Kari Ikonen <mr.kari.ikonen@gmail.com>
2020-10-30 21:51:52 +0200

    FIX experiment

--- src/PhysicsEngine.js
+++ src/PhysicsEngine.js
@@ -1,4 +1,4 @@
-const WORLD_SPEED = 20;
+const WORLD_SPEED = 40;
 
 const DEFAULT_GRAVITY = 0.9;
 const DEFAULT_FRICTION = 0.9;
@@ -12,35 +12,52 @@ const START_JUMP_VELOCITY = 20;
 const MIN_JUMP_VELOCITY = 0.5;
 
 const START_FALL_VELOCITY = 2;
+const MIN_FALL_VELOCITY = 0.01;
 const MAX_FALL_VELOCITY = 100;
+
+const NOP = function() {};
 
 
 export default class PhysicsEngine {
   constructor(dispatch) {
+    const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
+    methods
+      .filter(method => (method !== 'constructor'))
+      .forEach((method) => { this[method] = this[method].bind(this); });
+
     this.dispatch = dispatch;
 
     this.idBase = 0;
     this.items = new Map();
 
-    let self = this;
-    this.controls = {
-      nop: function() {},
-
-      ArrowLeft: function(ev) {
-        self.moveLeft(self.player);
-      },
-      ArrowRight: function(ev) {
-        self.moveRight(self.player);
-      },
-      ArrowUp: function(ev) {
-        self.jump(self.player);
-      },
+    this.area = {
+      x0: 0,
+      y0: 0,
+      x1: 0,
+      y1: 0,
     };
 
-    const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
-    methods
-      .filter(method => (method !== 'constructor'))
-      .forEach((method) => { this[method] = this[method].bind(this); });
+    this.controls = {
+      nop: NOP,
+      ArrowLeft: this.onLeft,
+      ArrowRight: this.onRight,
+      ArrowUp: this.onUp,
+      ArrowDown: this.onDown,
+    };
+
+  }
+
+  onLeft(ev) {
+    this.moveLeft(this.player);
+  }
+  onRight(ev) {
+    this.moveRight(this.player);
+  }
+  onUp(ev) {
+    this.jump(this.player);
+  }
+  onDown(ev) {
+    this.fall(this.player);
   }
 
   nextId() {
@@ -49,6 +66,19 @@ export default class PhysicsEngine {
 
   register(item, isPlayer) {
     item.id = this.nextId();
+
+    let halfW = item.width / 2;
+    let halfH = item.height / 2;
+
+    item.box = {
+      halfW: halfW,
+      halfH: halfH,
+      x0: item.x - halfW,
+      y0: item.y - halfH,
+      x1: item.x + halfW,
+      y1: item.y + halfH,
+    };
+
     this.items.set(item.id, item);
     if (isPlayer) {
       this.setPlayer(item.id);
@@ -104,9 +134,8 @@ export default class PhysicsEngine {
   }
 
   stopItem(item) {
-    this.stopFall(item);
-    this.stopJump(item);
-    this.stopMove(item);
+    item.velocityX = 0;
+    item.velocityY = 0;
   }
 
   startItem(item) {
@@ -114,83 +143,68 @@ export default class PhysicsEngine {
       item.itemChanged(item);
     }
 
-    if (item.isPlayer) {
-      this.fall(item);
-    }
+    this.testFall(item);
   }
 
   tick() {
-    const areaW = this.getWidth();
-    const areaH = this.getHeight();
+    this.area.x1 = this.getWidth();
+    this.area.y1 = this.getHeight();
 
-    this.items.forEach(function(item) { this.tickItem(item, areaW, areaH); }.bind(this));
+    this.hitDetection();
+
+    this.items.forEach(function(item) { this.tickItem(item); }.bind(this));
   }
 
-  tickItem(item, areaW, areaH) {
-    if (item.velocityX && item.velocityX > 0) {
-      const minX =  item.width / 2;
-      const maxX = areaW - item.width / 2;
+  hitDetection() {
+    this.items.forEach(function(item) {
+      this.itemHitDetection(item);
+    }.bind(this));
+  }
 
-      this.handleMove(item, minX, maxX);
+  itemHitDetection(item) {
+    let hits = [];
+    this.items.forEach(function(b) {
+      if (this.isHit(item, b)) {
+        hits.push(b);
+      }
+    }.bind(this));
+
+    if (hits.length) {
+    }
+  }
+
+  tickItem(item) {
+    if (item.velocityX && item.velocityX > 0) {
+      this.handleMove(item);
     }
 
     if (item.velocityY && item.velocityY > 0) {
-      const minY = item.height / 2;
-      const maxY = areaH - item.height / 2;
-
       if (item.dirY > 0) {
-        this.handleFall(item, minY, maxY);
+        this.handleFall(item);
       } else {
-        this.handleJump(item, minY, maxY);
+        this.handleJump(item);
       }
     }
   }
 
-  getMinX(item) {
-    return item.width / 2;
-  }
-
-  getMaxX(item) {
-    return this.getWidth() - item.width / 2;
-  }
-
-  getMinY(item) {
-    return item.height / 2;
-  }
-
-  getMaxY(item) {
-    return this.getHeight() - item.height / 2;
-  }
-
-  stopJump(item) {
-    item.velocityY = 0;
-  }
-
-  stopFall(item) {
-    item.velocityY = 0;
-  }
-
-  stopMove(item) {
-    item.velocityX = 0;
+  testFall(item) {
+    this.fall(item);
+    this.items.forEach(function(b) {
+    });
   }
 
   jump(item) {
-    if (item.velocityY && item.velocityY > 0) {
+    if (item.velocityY > 0) {
       return;
     }
 
-    this.stopFall(item);
-
-    item.gravity = item.gravity || DEFAULT_GRAVITY;
+    item.gravity = item.gravity != null ? item.gravity : DEFAULT_GRAVITY;
     item.velocityY = START_JUMP_VELOCITY;
     item.dirY = -1;
   }
 
   fall(item) {
-    this.stopJump(item);
-    this.stopFall(item);
-
-    item.gravity = item.gravity || DEFAULT_GRAVITY;
+    item.gravity = item.gravity != null ? item.gravity : DEFAULT_GRAVITY;
     item.velocityY = START_FALL_VELOCITY;
     item.dirY = 1;
   }
@@ -216,79 +230,132 @@ export default class PhysicsEngine {
       item.velocityX *= ADD_MOVE_VELOCITY;
     } else {
       item.velocityX = item.velocityX || START_MOVE_VELOCITY;
-      item.friction = item.friction || DEFAULT_FRICTION;
+      item.friction = item.friction != null ? item.friction : DEFAULT_FRICTION;
     }
     if (item.velocityX > MAX_MOVE_VELOCITY) {
       item.velocityX = MAX_MOVE_VELOCITY;
     }
   }
 
-  handleJump(item, minY, maxY) {
+  handleJump(item) {
+    if (item.velocityY === 0) {
+      return;
+    }
+
     item.velocityY = item.velocityY * item.gravity;
 
     let movement = item.velocityY * 1;
-    let newY = item.y + movement * item.dirY;
+    let diffY = movement * item.dirY;
 
-    if (newY <= minY) {
-      newY = minY;
-    } else if (newY > maxY) {
-      newY = maxY;
-    }
+    this.adjustLocation(item, 0, diffY);
 
-    if (newY <= minY || item.velocityY < MIN_JUMP_VELOCITY) {
+    if (item.box.y1 <= this.area.y0 || item.velocityY < MIN_JUMP_VELOCITY) {
       this.fall(item);
     }
-
-    item.y = newY;
 
     item.itemChanged(item);
   }
 
-  handleFall(item, minY, maxY) {
+  handleFall(item) {
+    if (item.velocityY === 0) {
+      return;
+    }
+
     item.velocityY = item.velocityY / item.gravity;
+
     if (item.velocityY > MAX_FALL_VELOCITY) {
       item.velocityY = MAX_FALL_VELOCITY;
     }
 
     let movement = item.velocityY * 1;
-    let newY = item.y + movement * item.dirY;
+    let diffY = movement * item.dirY;
 
-    if (newY <= minY) {
-      newY = minY;
-    } else if (newY > maxY) {
-      newY = maxY;
+    this.adjustLocation(item, 0, diffY);
+
+    if (item.box.y1 >= this.area.y1 || item.velocityY < MIN_FALL_VELOCITY) {
+      item.velocityY = 0;
     }
-
-    if (newY >= maxY) {
-      this.stopFall(item);
-    }
-
-    item.y = newY;
 
     item.itemChanged(item);
   }
 
-  handleMove(item, minX, maxX) {
+  handleMove(item) {
     item.velocityX = item.velocityX * item.friction;
 
     let movement = item.velocityX * 1;
     let newX = item.x + movement * item.dirX;
 
-    if (newX <= minX) {
-      newX = minX;
-    } else if (newX > maxX) {
-      newX = maxX;
-    }
+    this.adjustLocation(item, movement * item.dirX, 0);
 
     if (item.velocityX < MIN_MOVE_VELOCITY) {
-      this.stopMove(item);
-    } else if (newX <= minX || newX >= maxX) {
+      item.velocityX = 0;
+    } else if (item.box.x0 <= this.area.x0 || item.box.x1 >= this.area.x1) {
       item.dirX = item.dirX * -1;
       newX += item.dirX;
     }
 
-    item.x = newX;
-
     item.itemChanged(item);
+  }
+
+  adjustLocation(item, diffX, diffY) {
+    let changed = false;
+
+    let area = this.area;
+    let box = item.box;
+    let halfW = box.halfW;
+    let halfH = box.halfH;
+
+    if (diffX !== 0) {
+      let newX = item.x + diffX;
+
+      if (newX - halfW < area.x0) {
+        newX = area.x0 + halfW;
+      } else if (newX + halfW > area.x1) {
+        newX = area.x1 - halfW;
+      }
+
+      changed = item.x !== newX;
+      item.x = newX;
+    }
+
+    if (diffY !== 0) {
+      let newY = item.y + diffY;
+
+      if (newY - halfH < area.y0) {
+        newY = area.y0 + halfH;
+      } else if (newY + halfH > area.y1) {
+        newY = area.y1 - halfH;
+      }
+
+      changed = changed || item.y !== newY;
+      item.y = newY;
+    }
+
+    if (changed) {
+      box.x0 = item.x - halfW;
+      box.y0 = item.y - halfH;
+      box.x1 = item.x + halfW;
+      box.y1 = item.y + halfH;
+    };
+
+    return changed;
+  }
+
+  isHit(a, b) {
+    if (a === b) {
+      return false;
+    }
+
+    let boxA = a.box;
+    let boxB = b.box;
+
+    if (boxA.x1 < boxB.x0 || boxA.x0 > boxB.x1) {
+      return false;
+    }
+    if (boxA.y1 < boxB.y0 || boxA.y0 > boxB.y1) {
+      return false;
+    }
+
+    return true;
   }
 }
